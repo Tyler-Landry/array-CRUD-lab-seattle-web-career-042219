5e0cab4049cdef66d30717f502211cbb68ff3180
Tyler Landry <tyler.landry1996@outlook.com>
2019-04-18 00:23:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,9 +6,8 @@ def create_an_array
   list = ["Chungus","Chungus","Chungus","Shabungus"]
 end
 
-def add_element_to_end_of_array(list, "BIG CHUNGUS")
-  list = ["Chungus","Chungus","Chungus","Shabungus"]
-  list << "BIG CHUNGUS"
+def add_element_to_end_of_array(array, element)
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
